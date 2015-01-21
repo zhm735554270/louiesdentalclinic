b8003cc440fb2bb52004bbc51df973d38336177d
rafaelrofiles <rrofiles@gmail.com>
2015-01-21 08:34:12 +0800

    Update Louie's Dental Theme

--- sites/all/themes/louiesdentaltheme/templates/page.tpl.php
+++ sites/all/themes/louiesdentaltheme/templates/page.tpl.php
@@ -18,9 +18,9 @@
             <?php if ($top_bar_main_menu) :?>
               <?php print $top_bar_main_menu; ?>
             <?php endif; ?>
-            <?php if ($top_bar_secondary_menu) :?>
-              <?php print $top_bar_secondary_menu; ?>
-            <?php endif; ?>
+            <?php //if ($top_bar_secondary_menu) :?>
+              <?php //print $top_bar_secondary_menu; ?>
+            <?php //endif; ?>
           </section>
         </nav>
       <?php if ($top_bar_classes): ?>
